71e0c607f1b38fefb6b47529b60eff63f669053b
Todd Moy <todd.moy@gmail.com>
2018-12-12 09:52:42 -0700

    Initial commit

---
+++ example.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
