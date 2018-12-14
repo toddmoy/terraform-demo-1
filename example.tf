2bcfedc5a60d1fe49e7c0c91ab77fe557d4e9404
Todd Moy <22126+toddmoy@users.noreply.github.com>
2018-12-14 08:40:59 -0700

    Update example.tf

--- example.tf
+++ example.tf
@@ -7,9 +7,4 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-b374d5a5"
   instance_type = "t2.micro"
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
 }
-
